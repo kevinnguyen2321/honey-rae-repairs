f22cfa47bc93aa098333c7df3a70fd48274396a1
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-30 15:08:43 -0500

    Added click button and count state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
-}
+  const [count, setCount] = useState(0);
+
+  const handleBtnClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <div className="welcome">Welcome to your first React Application!</div>
+      <div className="btn-wrapper">
+        <button className="btn-secondary" onClick={handleBtnClick}>
+          Click me!
+        </button>
+      </div>
+      <div className="count-text">Count:{count}</div>
+    </>
+  );
+};
