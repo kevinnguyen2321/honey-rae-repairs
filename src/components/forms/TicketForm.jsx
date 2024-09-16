0e2600213b3e6e4024d5b26ed2918e6dc1bebbdc
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-09-16 10:53:49 -0500

    Add create ticket functionality

--- src/components/forms/TicketForm.jsx
+++ src/components/forms/TicketForm.jsx
@@ -1,3 +1,73 @@
-export const TicketForm = () => {
-    
-}
+import { useState } from 'react';
+import './Form.css';
+import { createTicket } from '../../services/ticketService';
+import { useNavigate } from 'react-router-dom';
+
+export const TicketForm = ({ currentUser }) => {
+  const [ticket, setTicket] = useState({ description: '', emergency: false });
+
+  const navigate = useNavigate();
+
+  const handleSave = (event) => {
+    event.preventDefault();
+    if (ticket.description) {
+      const newTicket = {
+        userId: currentUser.id,
+        description: ticket.description,
+        emergency: ticket.emergency,
+        dateCompleted: '',
+      };
+
+      createTicket(newTicket).then(() => {
+        navigate('/tickets');
+      });
+    } else {
+      window.alert('Please fill out the description!');
+    }
+  };
+
+  return (
+    <form>
+      <h2>New Service Ticket</h2>
+      <fieldset>
+        <div className="form-group">
+          <label>Description</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Brief description of problem"
+            onChange={(e) => {
+              const ticketCopy = { ...ticket };
+              ticketCopy.description = e.target.value;
+              setTicket(ticketCopy);
+            }}
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <label>
+            Emergency:
+            <input
+              type="checkbox"
+              onChange={(e) => {
+                const ticketCopy = { ...ticket };
+                ticketCopy.emergency = e.target.checked;
+                setTicket(ticketCopy);
+              }}
+            />
+          </label>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div className="form-group">
+          <button className="form-btn btn-info" onClick={handleSave}>
+            Submit Ticket
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
--- src/views/CustomerViews.jsx
+++ src/views/CustomerViews.jsx
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import { Welcome } from '../components/welcome/Welcome';
 import { CustomerNav } from '../components/nav/CustomerNav';
 import { TicketList } from '../components/tickets/TicketList';
+import { TicketForm } from '../components/forms/TicketForm';
 
 export const CustomerViews = ({ currentUser }) => {
   return (
@@ -18,7 +19,10 @@ export const CustomerViews = ({ currentUser }) => {
         <Route index element={<Welcome />} />
         <Route path="tickets">
           <Route index element={<TicketList currentUser={currentUser} />} />
-          <Route path="create" element={<>Ticket form</>} />
+          <Route
+            path="create"
+            element={<TicketForm currentUser={currentUser} />}
+          />
         </Route>
       </Route>
     </Routes>
